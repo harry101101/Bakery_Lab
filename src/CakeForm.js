580e420c1f2f1c9507b78d2e2941c1f7d609585c
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2023-01-23 16:23:23 +0000

    added alot of stuff

--- src/CakeForm.js
+++ src/CakeForm.js
@@ -1,13 +1,41 @@
+import { useState } from "react";
+
 const CakeForm = ({addNewCake}) => {
+
+    const [cakeName, setCakeName] = useState("");
+    const [ingredients, setIngredients] = useState("");
+    const [rating, setRating] = useState(0);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const cake = {
+            cakeName: cakeName,
+            ingredients: splitIngredients(ingredients),
+            rating: rating
+        };
+        console.log("ingredients " + cake.ingredients);
+    };
+
+    const splitIngredients = (ingredientString) => {
+        const ingredientArray = ingredientString.split(",");
+        return ingredientArray;
+    };
 
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="name">Cake Name:</label>
-            <input type="text" id="name" name="cakeName" onChange={(event) => setCakeName} />
+            <input type="text" id="name" name="cakeName"
+                   onChange={(event) => {setCakeName(event.target.value)}} 
+                   value={cakeName}/>
             <label htmlFor="ingredients">Ingredients:</label>
-            <textarea cols="30" rows="1" id="ingredients"></textarea>
+            <textarea cols="30" rows="1" 
+                    id="ingredients"
+                    placeholder="Enter a comma-separated list" 
+                    onChange={(event) =>{setIngredients(event.target.value)}} 
+                    value={ingredients} 
+                    name="ingredients"></textarea>
             <label htmlFor="rating">Rating:</label>
-            <input type="number" id="rating" min={1} max={5}/>
+            <input type="number" id="rating" min={1} max={5} onChange={(event) => {setRating(event.target.value)}} value={rating}/>
             <input type="submit"/>
         </form>
     )
